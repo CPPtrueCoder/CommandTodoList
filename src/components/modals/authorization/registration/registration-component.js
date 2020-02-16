a6ff4f41e8f5a26ca4cbb11eb2fbb1eab5be67a6
Andrew <andierodin@gmail.com>
2020-02-16 23:00:28 +0300

    Complete registration module"

--- src/components/modals/authorization/registration/registration-component.js
+++ src/components/modals/authorization/registration/registration-component.js
@@ -1,14 +1,15 @@
 import React,{Component} from "react";
-
+import './registration.sass'
 export default class RegistrationComponent extends Component{
 render() {
 
 return(
-    <form>
-        <input type="text" name="user_name" required/>
-        <input type="text" name="user_email" required/>
-        <input type="password" name="user_password" required/>
-
+    <form method="post" action="" className="regform">
+        <h2>Регистрация</h2>
+        <input type="text" className="itemform form-control-plaintext" name="user_name"  placeholder="Имя" required/>
+        <input type="text"  className="itemform form-control-plaintext" name="user_email" placeholder="Email" required/>
+        <input type="password"  className="itemform form-control-plaintext" name="user_password"  placeholder="Пароль" required/>
+        <input type="submit" className="btn btn-success itemform" value="Зарегистрироваться"/>
     </form>
 )
 }
